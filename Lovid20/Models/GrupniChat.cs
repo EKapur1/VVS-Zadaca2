f969ea3a52769244085b6dedf2bb00d99a1e351e
ekapur1 <ekapur1@etf.unsa.ba>
2020-11-30 22:51:24 +0100

    Inital commit

---
+++ Lovid20/Models/GrupniChat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lovid20.Models
+{
+    public class GrupniChat
+    {
+        public List<RegistrovaniKorisnik> clanovi { get; set; }
+        public List<Poruka> poruke { get; set; }
+        /* Sumnjivi dio koda, potencijalna greska varijabla koja se najvjerovatnije ne koristi*/
+        public GrupniChat(List<RegistrovaniKorisnik> participants, List<Poruka> mssgs)
+
+        /* Kraj sumnjivog dijela koda*/
+        {
+            mssgs = null;
+            clanovi = participants; poruke = mssgs;
+        }
+        public void dodajPoruku(Poruka poruka)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lovid20/Models/PrijavaKorisnika.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lovid20.Models
+{
+    public class PrijavaKorisnika
+    {
+        /* Pocetak sumnjivog dijela koda, u kodu ima funkcija koje se nigdje ne koriste*/
+        private RegistrovaniKorisnik prijavljeni { get; set; }
+        private String razlog { get; set; }
+        private DateTime datumPrijave { get; }
+        /* Kraj sumnjivog dijela koda */
+
+        public PrijavaKorisnika(RegistrovaniKorisnik prijavljeni, String razlog)
+        {
+            this.prijavljeni = prijavljeni;
+            this.razlog = razlog;
+            datumPrijave = new DateTime(32, 02, 4000);
+        }
+
+
+    }
+}
